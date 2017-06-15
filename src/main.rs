27a91b81c0b9fb4a2626060d78d7a9663a5f2cd5
Michael Hardy <98.michael.hardy@gmail.com>
2017-06-15 13:19:35 -0700

    Runs, but uses too much memory

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,30 @@ use std::io;//::{self, Write};
 use image::GenericImage;
 
 //should be a trait.  I am not sure how to impliment one for only Vec <Vec <bool>> and not Vec <T>
-fn boundless_insert(x: usize, y: usize, value: bool, img: &mut Vec<Vec <bool>>) {
+fn boundless_insert(x: i64, y: i64, value: bool, img: &mut Vec<Vec <bool>>) {
+	let mut tx = x;
+	let mut ty = y;
 	for i in 0..img.len(){
-		while img[i].len() <= x {
+		while img[i].len() <= tx as usize {
 			img[i].push(false);
+		}
+		while tx < 0 {
+			img[i].insert(0, false);
+			tx += 1;
 		}
 	}
 	let mut row:Vec <bool> = Vec::new();
-	for x in 0..img[0].len() {
+	for i in 0..img[0].len() {
 		row.push(false);
 	}
-	while img.len() <= y {
+	while img.len() <= ty as usize {
 		img.push(row.clone())
 	}
-	img[y][x] = value;
+	while ty < 0 {
+		img.insert(0, row.clone());
+		ty += 1;
+	}
+	img[ty as usize][tx as usize] = value;
 }
 
 struct ImgBlob {
@@ -45,25 +55,32 @@ impl ImgBlob {
 		while queue.len() > 0 {
 			let tempx = queue[0][1];
 			let tempy = queue[0][0];
-			if ((tempy) < img.len()) && ((tempx) < img[0].len()) {
-				if img[tempy][tempx] && !(claim[tempy][tempx ]){
-					boundless_insert((tempy-y), (tempx-x), true, &mut bitmap);
-					claim[(tempy-y)][(tempx-x)] = true;
+			if (tempy < img.len()) && (tempx < img[0].len()) && (tempy > 0) && (tempx > 0) {
+				println!("passed initial tests");
+				if img[tempy][tempx] && !(claim[tempy][tempx]){
+					println!("insert initiated");
+					println!("Insert: [{}, {}], [{}, {}]", x, tempx, y, tempy);
+					boundless_insert(((tempy as i64)-(y as i64)), ((tempx as i64)-(x as i64)), true, &mut bitmap);
+					println!("inserted");
+					claim[tempy][tempx] = true;
+					println!("Queue pushes started");
 					if queue[0][1] > y {
 						queue.push([tempy-1, tempx]);
 					}
 					queue.push([tempy, tempx-1]);
 					queue.push([tempy+1, tempx]);
 					queue.push([tempy, tempx+1]);
+					println!("Queue pushes finished");
 				}
 			}
 			queue.remove(0);
 		}
-		if bitmap[0].len() + y+bitmap.len() > 6 {
+		if bitmap[0].len() + bitmap.len() > 6 {
+			println!("Found: {} x {}", bitmap[0].len(), bitmap.len());
 			Some(ImgBlob {
 				blob_type: 0,
 				top_right: [x, y],
-				middle: [x+bitmap[0].len(), y+bitmap.len()],
+				middle: [x+(bitmap[0].len()/2), y+(bitmap.len()/2)],
 				text: String::from(""),
 				bitmap: bitmap
 			})
@@ -111,24 +128,22 @@ fn main() {
 	}
 	for y in 0..rthresh.len() {
 		for x in 0..rthresh[0].len() {
-			if rthresh[y][x] {
+			if rthresh[y][x] && (rthresh[y+1][x] || rthresh[y][x+1]) {
 				match ImgBlob::from_top_right(x, y, &mut claimed, &rthresh){
 					Some(o) => rblobs.push(o),
 					None => {},
 				}
-			} else if gthresh[y][x] {
+			}/* else if gthresh[y][x] && (gthresh[y+1][x] || gthresh[y][x+1]) {
 				match ImgBlob::from_top_right(x, y, &mut claimed, &gthresh){
 					Some(o) => gblobs.push(o),
 					None => {},
 				}
-			} else if bthresh[y][x] {
+			} else if bthresh[y][x]  && (bthresh[y+1][x] || bthresh[y][x+1]) {
 				match ImgBlob::from_top_right(x, y, &mut claimed, &bthresh){
 					Some(o) => bblobs.push(o),
 					None => {},
 				}
-			}
+			}*/
 		}
 	}
-	//let ref mut fout = File::create(&Path::new("outr.png")).unwrap();
-	//let _ = image::ImageLuma8(imgbuf).save(fout, image::PNG);
 }
