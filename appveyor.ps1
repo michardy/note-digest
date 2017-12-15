fbf6e2fba94e0501d1e2ea4f711a9243b4a880e5
Michael Hardy <98.michael.hardy@gmail.com>
2017-12-14 16:32:12 -0800

    Maybe Powershell is the answer

--- appveyor.ps1
+++ appveyor.ps1
@@ -4,3 +4,5 @@ $output = "$PSScriptRoot\rustup-init.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 rustup-init -yv --default-toolchain %channel% --default-host %target%
 $env:Path += ";%USERPROFILE%\.cargo\bin"
+
+cargo test
