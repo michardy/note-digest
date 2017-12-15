4f245ea225743ec6cdff95701e77ddd2051d2525
Michael Hardy <98.michael.hardy@gmail.com>
2017-12-14 17:16:51 -0800

    The build hang strikes back

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,8 +1,14 @@
 $url = "https://win.rustup.rs"
 $output = "$PSScriptRoot\rustup-init.exe"
 
+echo "Running setup:"
+echo ""
+echo "Downloading..."
+
 Invoke-WebRequest -Uri $url -OutFile $output
-rustup-init -yv --default-toolchain %channel% --default-host %target%
+
+echo "Initializing rustup..."
+rustup-init -yv
 $env:Path += ";%USERPROFILE%\.cargo\bin"
 
 cargo test
