0aefe97205a20b06fc748a71bcd803d25b3090d8
Michael Hardy <98.michael.hardy@gmail.com>
2017-12-24 23:33:18 -0800

    Changes to templates and precompilation

--- build.rs
+++ build.rs
@@ -1,12 +1,10 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-const TC_INDEX: &'static str = "./src/templates/table/index.html";
-const TC_INDEX_OUT: &'static str = "./src/template_fragments/table/index.html";
-const C_INDEX: &'static str = "./src/templates/chapter/index.html";
-const C_INDEX_OUT: &'static str = "./src/template_fragments/chapter/index.html";
-const C_GEN: &'static str = "./src/templates/chapter/gen.css";
-const C_GEN_OUT: &'static str = "./src/template_fragments/chapter/gen.css";
+const INDEX: &'static str = "./src/templates/chapter/index.html";
+const INDEX_OUT: &'static str = "./src/template_fragments/chapter/index.html";
+const GEN: &'static str = "./src/templates/chapter/gen.css";
+const GEN_OUT: &'static str = "./src/template_fragments/chapter/gen.css";
 
 fn process_file(fname: &'static str, oname: &'static str) {
 	let mut f = File::open(fname).expect(
@@ -29,7 +27,6 @@ fn process_file(fname: &'static str, oname: &'static str) {
 }
 
 fn main() {
-	process_file(TC_INDEX, TC_INDEX_OUT);
-	process_file(C_INDEX, C_INDEX_OUT);
-	process_file(C_GEN, C_GEN_OUT);
+	process_file(INDEX, INDEX_OUT);
+	process_file(GEN, GEN_OUT);
 }
