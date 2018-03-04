e21e7e823587d21dca25df0edb875f4262675b56
Michael Hardy <98.michael.hardy@gmail.com>
2018-03-03 17:37:54 -0800

    Added fullscreen

--- src/templates/chapter/util.js
+++ src/templates/chapter/util.js
@@ -3,3 +3,16 @@ function adjust_size(cont) {
 	var view = document.getElementsByTagName('main')[0];
 	view.style.width = s;
 }
+function req_fullscreen() {
+	// based on https://stackoverflow.com/questions/3900701/onclick-go-full-screen/3900711#3900711
+	var elem = document.body;
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	} else if (elem.msRequestFullscreen) {
+		elem.msRequestFullscreen();
+	} else if (elem.mozRequestFullScreen) {
+		elem.mozRequestFullScreen();
+	} else if (elem.webkitRequestFullscreen) {
+		elem.webkitRequestFullscreen();
+	}
+}
--- src/templates/table/util.js
+++ src/templates/table/util.js
@@ -3,6 +3,19 @@ function adjust_size(cont) {
 	var view = document.getElementsByTagName('main')[0];
 	view.style.width = s;
 }
+function req_fullscreen() {
+	// based on https://stackoverflow.com/questions/3900701/onclick-go-full-screen/3900711#3900711
+	var elem = document.body;
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	} else if (elem.msRequestFullscreen) {
+		elem.msRequestFullscreen();
+	} else if (elem.mozRequestFullScreen) {
+		elem.mozRequestFullScreen();
+	} else if (elem.webkitRequestFullscreen) {
+		elem.webkitRequestFullscreen();
+	}
+}
 function show_sub(obj) {
 	
 }
