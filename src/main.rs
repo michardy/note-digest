577cab3bedf214d3869c996d2eb3479177d8caa6
Michael Hardy <98.michael.hardy@gmail.com>
2018-03-26 00:35:42 -0700

    Adjusted a contant and found out diagonal checks were doing something
    They are now back on

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ const MIN_THRESH: u8 = 120;
 const MAX_THRESH: u8 = 126;
 
 /// Minimum width to heigth ratio for object to be considered a line
-const LINE_RATIO: f32 = 3.0;
+const LINE_RATIO: f32 = 4.0;
 
 /// Defines red channel index
 const RED: u8 = 0;
@@ -141,10 +141,14 @@ impl ImgBlob {
 					}
 					claim[tempy][tempx] = true;
 					if tempy > top {
+						queue.push([tempy-1, tempx-1]);
 						queue.push([tempy-1, tempx]);
+						queue.push([tempy-1, tempx+1]);
 					}
 					queue.push([tempy, tempx-1]);
+					queue.push([tempy+1, tempx-1]);
 					queue.push([tempy+1, tempx]);
+					queue.push([tempy+1, tempx+1]);
 					queue.push([tempy, tempx+1]);
 				}
 			}
