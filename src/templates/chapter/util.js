b0a99111ea0174cd5cdc158a54b8b37714b41933
Michael Hardy <98.michael.hardy@gmail.com>
2018-04-25 22:42:17 -0700

    Fixed #18

--- src/templates/chapter/util.js
+++ src/templates/chapter/util.js
@@ -1,7 +1,9 @@
 function adjust_size(cont) {
-	var s = cont.value;
+	var s = Number(cont.value.substring(0,cont.value.length-1));
 	var view = document.getElementsByTagName('main')[0];
-	view.style.width = s;
+	var scale = s/100;
+	var trans = (s-100)/2;
+	view.style.transform = "translate("+trans+"%, "+trans+"%) scale("+scale+")";
 }
 function req_fullscreen() {
 	// based on https://stackoverflow.com/questions/3900701/onclick-go-full-screen/3900711#3900711
