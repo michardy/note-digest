a0bf43c48fe200d9f87d5ff38511516fd0a200ee
Michael Hardy <98.michael.hardy@gmail.com>
2018-04-26 01:05:08 -0700

    Impliment color toggle

--- src/templates/table/util.js
+++ src/templates/table/util.js
@@ -5,6 +5,41 @@ function adjust_size(cont) {
 	var trans = (s-100)/2;
 	view.style.transform = "translate("+trans+"%, "+trans+"%) scale("+scale+")";
 }
+function color(box) {
+	if (box.checked) {
+		var heads = document.getElementsByClassName('head');
+		while (heads.length > 0) {
+			heads[0].classList.add('hiddenred');
+			heads[0].classList.remove('head');
+		}
+		var defines = document.getElementsByClassName('defi');
+		while (defines.length > 0) {
+			defines[0].classList.add('hiddengreen');
+			defines[0].classList.remove('defi');
+		}
+		var contents = document.getElementsByClassName('cont');
+		while (contents.length > 0) {
+			contents[0].classList.add('hiddenblue');
+			contents[0].classList.remove('cont');
+		}
+	} else {
+		var hiddenreds = document.getElementsByClassName('hiddenred');
+		while (hiddenreds.length > 0) {
+			hiddenreds[0].classList.add('head');
+			hiddenreds[0].classList.remove('hiddenred');
+		}
+		var hiddengreens = document.getElementsByClassName('hiddengreen');
+		while (hiddengreens.length > 0) {
+			hiddengreens[0].classList.add('defi');
+			hiddengreens[0].classList.remove('hiddengreen');
+		}
+		var hiddenblues = document.getElementsByClassName('hiddenblue');
+		while (hiddenblues.length > 0) {
+			hiddenblues[0].classList.add('cont');
+			hiddenblues[0].classList.remove('hiddenblue');
+		}
+	}
+}
 function req_fullscreen() {
 	// based on https://stackoverflow.com/questions/3900701/onclick-go-full-screen/3900711#3900711
 	var elem = document.body;
